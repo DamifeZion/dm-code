cf1bc257162473406298ea050acc7b625734d5d2
Damife Zion <damifeolaleye@gmail.com>
2023-08-24 17:50:01 +0100

    minor refactoring

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,20 +1,10 @@
-import { useSelector, useDispatch } from "react-redux"
-import { toggleSlice } from "../../redux/slices/exportSlices";
+
 
 const Navbar = () => {
 
-  const dispatch = useDispatch();
-  const menu = useSelector(state => state.toggleSlice.menu);
-
-
-  function handleClick(){
-    dispatch(toggleSlice.actions.toggleMenu())
-    console.log(menu)
-  }
-
   return (
     <div className='flex justify-between bg-[--color2] text-[--color1] px-[--px] py-[--py] text-xl font-black tracking-wider'>
-      <h1 onClick={handleClick}>Logo</h1>
+      <h1>Logo</h1>
 
       <div className=' flex'>
 
