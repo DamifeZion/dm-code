b91e2c323423af44826915a4f9d47a91a51a1681
Damife Zion <damifeolaleye@gmail.com>
2023-08-25 19:19:35 +0100

    minor refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {Navbar} from './components/exportComponent';
 import {Home, Docs} from './pages/exportPages';
 
+import { useTheme, useMediaQuery } from '@mui/material';
+
 function App() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('1060'));
+  
+  const mobileView = () => {
+    return <div className=' flex items-center justify-center w-full h-screen'>
+      <h1 className=' text-center font-black text-lg '>This Site Is Only Available On Desktops</h1>
+    </div>
+  }
 
   return (
     <section>
-      <BrowserRouter>
+      {
+      !isMobile ? <BrowserRouter>
 
         <div className='sticky top-0 z-[100]'>
           <Navbar />
@@ -15,13 +26,14 @@ function App() {
 
         <Routes>
           <Route path='/'
-           element={<Home/>}/>
+          element={<Home/>}/>
 
           <Route path='/docs'
-           element={<Docs />}/>
+          element={<Docs />}/>
         </Routes>
 
-      </BrowserRouter>
+      </BrowserRouter> : mobileView()
+}
     </section>
     
   )
